f0209e59de065e6b46f3ab48565427d6bbead28d
Mäkeli <joel.m.maenpaa@gmail.com>
2021-03-03 14:34:17 +0200

    prettier story

--- src/stories/Nav.stories.js
+++ src/stories/Nav.stories.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import { Nav } from '../components/Nav'
+import React from "react";
+import { Nav } from "../components/Nav";
 
 export default {
-    title: 'Components/Nav',
-    component: Nav
-}
+  title: "Components/Nav",
+  component: Nav,
+};
 
-export const APICall = () => <Nav />
+export const APICall = () => <Nav />;
