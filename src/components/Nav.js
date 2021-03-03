d968e53fa09d15eee11c6e11cd188bfe7cd765b1
Mäkeli <joel.m.maenpaa@gmail.com>
2021-03-03 14:34:23 +0200

    pretty much doen

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,40 +1,76 @@
-import React from 'react'
-import { Toolbar, Container, List, ListItem, ListItemText, makeStyles, Button } from '@material-ui/core'
+import React from "react";
+import {
+  Toolbar,
+  Container,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        backgroundColor: theme.palette.primary.main
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.primary.main,
+  },
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  list: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  button: {
+    color: theme.palette.textPrimary.main,
+    textDecoration: "none",
+    "&:hover": {
+      color: theme.palette.textPrimary.light,
+      cursor: "pointer",
+      textDecoration: "none",
     },
-    container: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between"
-    },
-    list: {
-        display: "flex",
-        flexDirection: "row"
-    }
-}))
+  },
+}));
 
 export const Nav = () => {
-    const { root, container, list } = useStyles()
+  const { root, container, list, button } = useStyles();
 
-    return (
-        <Toolbar className={root}>
-            <Container maxWidth="xl" className={container}>
-                <Button>EPO</Button>
-                <List dense className={list}>
-                    <ListItem button>
-                        <ListItemText primary="Osakunta" />
-                    </ListItem>
-                    <ListItem button>
-                        <ListItemText primary="Jäseneksi" />
-                    </ListItem>
-                    <ListItem button>
-                        <ListItemText primary="Linkkejä" />
-                    </ListItem>
-                </List>
-            </Container>
-        </Toolbar>
-    )
-}
+  const paths = [
+    {
+      name: "Osakunta",
+      path: "/osakunta",
+    },
+    {
+      name: "Jäseneksi",
+      path: "/jaseneksi",
+    },
+    {
+      name: "Linkkejä",
+      path: "/linkkeja",
+    },
+  ];
+
+  return (
+    <Toolbar className={root}>
+      <Container maxWidth="xl" className={container}>
+        <Link style={{ textDecoration: "none" }} to="/">
+          <Typography className={button}>EPO</Typography>
+        </Link>
+        <List dense className={list}>
+          {paths.map(({ name, path }, index) => {
+            return (
+              <Link id={index} style={{ textDecoration: "none" }} to={path}>
+                <ListItem className={button}>
+                  <ListItemText primary={name} />
+                </ListItem>
+              </Link>
+            );
+          })}
+        </List>
+      </Container>
+    </Toolbar>
+  );
+};
