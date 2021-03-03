f32f09523057847ff0b92efd267a44b56f7278bc
Mäkeli <joel.m.maenpaa@gmail.com>
2021-03-03 12:16:33 +0200

    storybook and sample story

---
+++ src/stories/Sample.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from '@material-ui/core'
+
+export const Sample = () => {
+    return (
+        <Container maxWidth="md">
+            Sample component
+        </Container>
+    )
+}
