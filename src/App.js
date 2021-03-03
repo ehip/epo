6323a13bf330637d48cbc60f6936df9748e36690
Mäkeli <joel.m.maenpaa@gmail.com>
2021-03-03 13:42:15 +0200

    added nav to app and theming

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// components
+import { Nav } from "./components/Nav";
 
 // views
-import { Home } from './views/Home'
+import { Home } from "./views/Home";
+
+// theming
+import { theme } from './theme'
+import { ThemeProvider } from '@material-ui/core'
 
 function App() {
   return (
-    <Router>
-      <Route exact path="/" component={Home} />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Nav />
+        <Route exact path="/" component={Home} />
+      </Router>
+    </ThemeProvider>
   );
 }
 
