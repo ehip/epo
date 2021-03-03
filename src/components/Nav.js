6c45d636a72b69c7bd478035d64df507931add66
Mäkeli <joel.m.maenpaa@gmail.com>
2021-03-03 13:42:03 +0200

    basic nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,40 @@
 import React from 'react'
-import { Toolbar } from '@material-ui/core'
+import { Toolbar, Container, List, ListItem, ListItemText, makeStyles, Button } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        backgroundColor: theme.palette.primary.main
+    },
+    container: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    list: {
+        display: "flex",
+        flexDirection: "row"
+    }
+}))
 
 export const Nav = () => {
+    const { root, container, list } = useStyles()
+
     return (
-        <Toolbar>
-            
+        <Toolbar className={root}>
+            <Container maxWidth="xl" className={container}>
+                <Button>EPO</Button>
+                <List dense className={list}>
+                    <ListItem button>
+                        <ListItemText primary="Osakunta" />
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemText primary="Jäseneksi" />
+                    </ListItem>
+                    <ListItem button>
+                        <ListItemText primary="Linkkejä" />
+                    </ListItem>
+                </List>
+            </Container>
         </Toolbar>
     )
 }
