0425c56c6d477783c00170ea8f3db4aca36f1e1e
Mäkeli <joel.m.maenpaa@gmail.com>
2021-03-03 13:40:49 +0200

    changed to hex codes cause yea

--- src/theme.js
+++ src/theme.js
@@ -1,17 +1,18 @@
 import { createMuiTheme } from '@material-ui/core/styles'
-import { red, grey } from '@material-ui/core/colors'
 
 const theme = createMuiTheme({
     palette: {
         primary: {
-            main: red['A700'],
+            main: '#ff5131',
             light: '#ff5132',
             dark: '#9b0000',
         },
         secondary: {
-            main: grey[900],
+            main: '#212121',
             light: '#484848',
             dark: '#000000',
         }
     },
 })
+
+export { theme }
