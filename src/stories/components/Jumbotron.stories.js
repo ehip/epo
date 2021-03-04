e33bad0952090ab02acf5caa540d5382fb54815c
Mäkeli <joel.m.maenpaa@gmail.com>
2021-03-04 12:11:56 +0200

    relocated component stories and such

---
+++ src/stories/components/Jumbotron.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Jumbotron } from "../../components/Jumbotron";
+
+export default {
+  title: "Components/Jumbotron",
+  component: Jumbotron,
+};
+
+export const APICall = () => <Jumbotron />;
---
+++ src/stories/components/Text.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text } from "../../components/Text";
+
+export default {
+  title: "Components/Text",
+  component: Text,
+};
+
+export const APICall = () => <Text />;
