eec592c576720d3b2013ea14892dff9582cf37b8
Mäkeli <joel.m.maenpaa@gmail.com>
2021-03-04 11:41:33 +0200

    prettierrrrrr

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import { Paper, Typography, makeStyles } from '@material-ui/core'
+import React from "react";
+import { Paper, Typography, makeStyles } from "@material-ui/core";
 
-import img from '../assets/img/jumbotron.jpeg'
+import img from "../assets/img/jumbotron.jpeg";
 
 const useStyles = makeStyles({
-    root: {
-        backgroundImage: `url(${img})`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        height: 300,
-        maxHeight: 300,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        WebkitTextStroke: "0.5px white"
-    }
-})
+  root: {
+    backgroundImage: `url(${img})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    height: 300,
+    maxHeight: 300,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    WebkitTextStroke: "0.5px white",
+  },
+});
 
 export const Jumbotron = () => {
-    const { root } = useStyles()
+  const { root } = useStyles();
 
-    return (
-        <Paper square elevation="0" className={root}>
-            <Typography variant="h2" align="center">
-                OULUN ETELÄ- JA KESKIPOHJALAINEN OSAKUNTA 
-            </Typography>
-        </Paper>
-    )
-}
+  return (
+    <Paper square elevation="0" className={root}>
+      <Typography variant="h2" align="center">
+        OULUN ETELÄ- JA KESKIPOHJALAINEN OSAKUNTA
+      </Typography>
+    </Paper>
+  );
+};
