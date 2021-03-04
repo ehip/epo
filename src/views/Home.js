3fbe8442ca907d372fe020fed137b267d2e031f2
Mäkeli <joel.m.maenpaa@gmail.com>
2021-03-04 11:43:11 +0200

    slap jumbotron to landing page

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,13 @@
 import React from "react";
-import { Container } from "@material-ui/core";
+import { Box } from "@material-ui/core";
+
+// components
+import { Jumbotron } from "../components/Jumbotron";
 
 export const Home = () => {
-  return 'EPO'
+  return (
+    <Box>
+      <Jumbotron />
+    </Box>
+  );
 };
