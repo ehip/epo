a959e572afcac0b8a49788da5342e57ca61b0c3b
Joel Mäenpää <zoloppe4lyf@gmail.com>
2021-03-05 11:59:28 +0200

    slap a container there

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Paper, Typography } from '@material-ui/core'
+import React from "react";
+import { Container, Paper, Typography } from "@material-ui/core";
 
 export const Text = ({ text }) => {
-    return (
-        <Paper>
-            <Typography>
-                {text}
-            </Typography>
-        </Paper>
-    )
-}
+  return (
+    <Container maxWidth="sm">
+      <Paper square elevation="0">
+        <Typography>{text}</Typography>
+      </Paper>
+    </Container>
+  );
+};
