a2d9fa758febf61411ceb0db569e54e5d9490cff
Joel Mäenpää <zoloppe4lyf@gmail.com>
2021-03-09 22:35:00 +0200

    slap text to homepage cba to work on this today

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -3,7 +3,7 @@ import { Container, Paper, Typography } from "@material-ui/core";
 
 export const Text = ({ text }) => {
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="md">
       <Paper square elevation="0">
         <Typography>{text}</Typography>
       </Paper>
--- src/views/Home.js
+++ src/views/Home.js
@@ -3,11 +3,15 @@ import { Box } from "@material-ui/core";
 
 // components
 import { Jumbotron } from "../components/Jumbotron/Jumbotron";
+import { Text } from "../components/Text/Text"
 
 export const Home = () => {
+  const text = "Oulun Etelä- ja Keskipohjalainen osakunta eli EPO ry toimii pohjalaishenkisten opiskelijoiden yhteisenä järjestönä. EPO järjestää jäsenilleen kaikenlaista hauskaa tekemistä erilaisten tapahtumien ja aktiviteettien muodossa. Suosittuja EPO:n tapahtumia ovat esimerkiksi erilaiset illanvietot, joissa katsotaan elokuvia ja pelataan lautapelejä, lisäksi jokavuotiset pikkujoulut ja kesällä tapahtuvat excursiot ovat jäsenistön suosiossa. Tule rohkeasti mukaan hauskaan ja yhteisölliseen porukkaan!"
+
   return (
     <Box>
       <Jumbotron />
+      <Text text={text} />
     </Box>
   );
 };
