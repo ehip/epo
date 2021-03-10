75cb585e760b473cad78595b32b295ebde50cfe2
Joel Mäenpää <zoloppe4lyf@gmail.com>
2021-03-10 13:31:54 +0200

    footer story

---
+++ src/stories/components/Footer.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Footer } from "../../components/Footer/Footer";
+
+export default {
+  title: "Components/Footer",
+  component: Footer,
+};
+
+export const APICall = () => <Footer />;
