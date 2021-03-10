6d38d2add386d298e99a4dab692fa7e316f4342a
Joel Mäenpää <zoloppe4lyf@gmail.com>
2021-03-10 13:06:49 +0200

    text styling

--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { Container, Paper, Typography } from "@material-ui/core";
+import { Container, Paper, Typography, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    padding: 10,
+    margin: "auto 5 auto 5",
+    textAlign: "justify!important"
+  }
+})
 
 export const Text = ({ text }) => {
+  const { root } = useStyles()
+
   return (
-    <Container maxWidth="md">
+    <Container maxWidth="md" className={root}>
       <Paper square elevation="0">
-        <Typography>{text}</Typography>
+        <Typography variant="body1">{text}</Typography>
       </Paper>
     </Container>
   );
