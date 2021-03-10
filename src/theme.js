083a41a9b865b8b3c62049ad50653dfd7bcd35d4
Joel Mäenpää <zoloppe4lyf@gmail.com>
2021-03-10 15:24:36 +0200

    prettier

--- src/theme.js
+++ src/theme.js
@@ -1,35 +1,35 @@
-import { createMuiTheme } from '@material-ui/core/styles'
+import { createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: 'rgb(188, 22, 22)',
-            light: '#ff5132',
-            dark: '#9b0000',
-        },
-        secondary: {
-            main: '#212121',
-            light: '#484848',
-            dark: '#000000',
-        },
-        textPrimary: {
-            main: '#ffffff',
-            light: '#c7c7c7'
-        }
+  palette: {
+    primary: {
+      main: "rgb(188, 22, 22)",
+      light: "#ff5132",
+      dark: "#9b0000",
     },
-    typography: {
-        h2: {
-            "@media (max-width:600px)": {
-                fontSize: "3rem"
-            },
-            "@media (max-width:470px)": {
-                fontSize: "2rem"
-            },
-            "@media (max-width:280px)": {
-                fontSize: "1.5rem"
-            }
-        },
-    }
-})
+    secondary: {
+      main: "#212121",
+      light: "#484848",
+      dark: "#000000",
+    },
+    textPrimary: {
+      main: "#ffffff",
+      light: "#c7c7c7",
+    },
+  },
+  typography: {
+    h2: {
+      "@media (max-width:600px)": {
+        fontSize: "3rem",
+      },
+      "@media (max-width:470px)": {
+        fontSize: "2rem",
+      },
+      "@media (max-width:280px)": {
+        fontSize: "1.5rem",
+      },
+    },
+  },
+});
 
-export { theme }
+export { theme };
