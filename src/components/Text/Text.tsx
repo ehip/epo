b1bab6682f75c574f54565093c0caefb26b58653
Joel Mäenpää <zoloppe4lyf@gmail.com>
2021-03-11 23:15:13 +0200

    first component to ts

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,20 +1,23 @@
-import React from "react";
 import { Container, Paper, Typography, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
     padding: 10,
     margin: "auto 5 auto 5",
-    textAlign: "justify!important",
+    textAlign: "justify",
   },
 });
 
-export const Text = ({ text }) => {
+interface Props {
+    text: string
+}
+
+export const Text = ({ text }: Props) => {
   const { root } = useStyles();
 
   return (
     <Container maxWidth="sm" className={root}>
-      <Paper square elevation="0">
+      <Paper square elevation={0}>
         <Typography variant="body1">{text}</Typography>
       </Paper>
     </Container>
