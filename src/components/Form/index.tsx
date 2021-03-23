ca7754d4bd6d246fc9d05cd03f9ed678dd057c11
Mäkeli <joel.m.maenpaa@gmail.com>
2021-03-23 18:16:02 +0200

    form pretty much done

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,30 +1,146 @@
 import { ChangeEvent, useState } from "react";
-import { Checkbox, Grid, TextField } from "@material-ui/core";
+import {
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  TextField,
+} from "@material-ui/core";
+
+// styles
+import { useStyles } from "./styles";
+
+// interfaces
+import { Lomake } from "../../data/interfaces/Lomake";
 
 export const Form = () => {
-  const [checked, setChecked] = useState(false)
+  const { root, checkbox } = useStyles();
+  const [checked, setChecked] = useState<boolean>(false);
+  const [iframe, setIFrame] = useState<boolean>(false)
+  const [form, setForm] = useState<Lomake>({
+    etunimi: "",
+    sukunimi: "",
+    kotipaikka: "",
+    sahkoposti: "",
+  });
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked)
+  const handleCheckChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleButtonClick = () => {
+    setIFrame(true)
   }
 
   return (
-    <form noValidate autoComplete="off">
+    <form
+      className={root}
+      method="post"
+      action="https://www.oulu.fi/cgi-bin/FormMail.pl"
+      target="formIFrame"
+      noValidate
+      autoComplete="off"
+    >
+      <input
+        type="hidden"
+        name="recipient"
+        value="709f794d8f7015ea46c8b50107e93c8dxx"
+      />
       <Grid container direction="column" spacing={1}>
         <Grid item xs>
-          <TextField required fullWidth label="Etunimi" variant="outlined" />
+          <TextField
+            InputLabelProps={{
+              style: {
+                color: "gray",
+              },
+            }}
+            required
+            fullWidth
+            name="etunimi"
+            label="Etunimi"
+            placeholder="Etunimi"
+            variant="outlined"
+            value={form.etunimi}
+            onChange={handleInputChange}
+          />
         </Grid>
         <Grid item xs>
-          <TextField required fullWidth label="Sukunimi" variant="outlined" />
+          <TextField
+            InputLabelProps={{
+              style: {
+                color: "gray",
+              },
+            }}
+            required
+            fullWidth
+            name="sukunimi"
+            label="Sukunimi"
+            placeholder="Sukunimi"
+            variant="outlined"
+            value={form.sukunimi}
+            onChange={handleInputChange}
+          />
         </Grid>
         <Grid item xs>
-          <TextField required fullWidth label="Kotipaikka" variant="outlined" />
+          <TextField
+            InputLabelProps={{
+              style: {
+                color: "gray",
+              },
+            }}
+            required
+            fullWidth
+            name="kotipaikka"
+            label="Kotipaikka"
+            placeholder="Kotipaikka"
+            variant="outlined"
+            value={form.kotipaikka}
+            onChange={handleInputChange}
+          />
         </Grid>
         <Grid item xs>
-          <TextField required fullWidth label="Sähköposti" variant="outlined" />
+          <TextField
+            InputLabelProps={{
+              style: {
+                color: "gray",
+              },
+            }}
+            required
+            fullWidth
+            name="sahkoposti"
+            label="Sähköposti"
+            placeholder="Sähköposti"
+            variant="outlined"
+            value={form.sahkoposti}
+            onChange={handleInputChange}
+          />
         </Grid>
         <Grid item xs>
-          <Checkbox checked={checked} onChange={handleChange} color="secondary" />
+          <FormControlLabel
+            control={
+              <Checkbox
+                className={checkbox}
+                checked={checked}
+                onChange={handleCheckChange}
+                name="checked"
+              />
+            }
+            label="Liity sähköpostilistalle"
+          />
+        </Grid>
+        <Grid item xs>
+          <Button variant="contained" color="primary" onClick={handleButtonClick} type="submit">
+            Lähetä
+          </Button>
+        </Grid>
+        <Grid item xs style={{width: "100%"}}>
+          <iframe title="iframe" style={{border: 0, display: iframe ? "block" : "none" }} name="formIFrame">
+          </iframe>
         </Grid>
       </Grid>
     </form>
---
+++ src/components/Form/styles.tsx
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/core";
+
+export const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: "center",
+    margin: "10px 0",
+  },
+  checkbox: {
+      color: theme.palette.primary.main,
+  }
+}));
