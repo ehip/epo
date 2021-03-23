fa5c9099cf82f7494c8e899d8e3b3b4ce2d204f7
Mäkeli <joel.m.maenpaa@gmail.com>
2021-03-23 18:59:05 +0200

    render a bunch of links

---
+++ src/data/interfaces/Linkki.ts
@@ -0,0 +1,8 @@
+export interface Linkki {
+    name: string;
+    link: string;
+}
+
+export interface Linkit {
+    links: Linkki[];
+}
--- src/pages/Linkkeja/index.tsx
+++ src/pages/Linkkeja/index.tsx
@@ -1,5 +1,50 @@
-import { Container } from "@material-ui/core"
+import { Container, Grid, Typography } from "@material-ui/core";
+
+// interfaces
+import { Linkki, Linkit } from "../../data/interfaces/Linkki";
+
+// json
+import linkit from "../../data/json/linkit.json";
 
 export const Linkkeja = () => {
-    return <Container>linkkejä</Container>
-}
+  // imagine this is the first thing that comes to your mind
+  // holy shit i'm such a flaming piece of garbage of a programmer
+  const links: Linkit[] = linkit;
+  const epoLinks: Linkki[] = links[0].links;
+  const otherLinks: Linkki[] = links[1].links;
+
+  return (
+    <Container maxWidth="lg">
+      <Grid container direction="column" justify="center" alignItems="center">
+        <Grid item>
+          <Typography variant="h2">Hyödyllisiä linkkejä</Typography>
+        </Grid>
+        {epoLinks.map((link, index) => {
+          return (
+            <Grid item>
+              <Typography variant="body1">
+                <a key={index} href={link.link}>
+                  {link.name}
+                </a>
+              </Typography>
+            </Grid>
+          );
+        })}
+        <Grid item>
+          <Typography variant="h4">Muita osakuntia</Typography>
+        </Grid>
+        {otherLinks.map((link, index) => {
+          return (
+            <Grid item>
+              <Typography variant="body1">
+                <a key={index} href={link.link}>
+                  {link.name}
+                </a>
+              </Typography>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
+  );
+};
