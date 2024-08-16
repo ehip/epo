8fdaf97203cd5fe8e2e779702ec60f65011516dc
ehip <156508492+ehip@users.noreply.github.com>
2024-08-16 09:50:53 +0300

    Ilmoitus uusista verkkosivuista

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,7 +5,9 @@ import { Jumbotron } from "../../components/Jumbotron";
 import { Text } from "../../components/Text";
 
 export const Home = () => {
+  
   const text: string =
+    "Nämä ovat epon vanhat sivut! Uudet verkkosivut ja liittymislomake löytyvät osoitteesta: https://sites.google.com/view/oulunepo "
     "Oulun Etelä- ja Keskipohjalainen osakunta eli EPO ry toimii pohjalaishenkisten opiskelijoiden yhteisenä järjestönä. EPO järjestää jäsenilleen kaikenlaista hauskaa tekemistä erilaisten tapahtumien ja aktiviteettien muodossa. Suosittuja EPO:n tapahtumia ovat esimerkiksi erilaiset illanvietot, joissa katsotaan elokuvia ja pelataan lautapelejä, lisäksi jokavuotiset pikkujoulut ja kesällä tapahtuvat excursiot ovat jäsenistön suosiossa. Tule rohkeasti mukaan hauskaan ja yhteisölliseen porukkaan!";
 
   return (
